de3bdb6da90d4e9610b7d6226bad7fe8e53f62bc
MariCruz <maricruzdev@gmail.com>
2023-12-08 21:25:55 -0300

    atv softex

---
+++ src/app/components/fruit.tsx
@@ -0,0 +1,103 @@
+"use client"
+
+import React, {useState} from "react";
+import {listFruit} from "./listFruit";
+
+
+
+export default function Fruit() {
+    const [fruit, setFruit] = useState(0);
+    const hasNext = fruit < listFruit.length - 1
+    
+
+    
+ function handleClick () {
+    if(hasNext) {
+      setFruit(fruit + 1);
+
+    } else{
+      setFruit(0);
+
+    }
+  
+}
+
+let tropicalFruit = listFruit[fruit];
+return (
+  <>
+    <button className= "bg-sky-500 px-4 py-2" onClick={handleClick}>
+      Next
+    </button>
+    <h2>
+      <i>{tropicalFruit.name} </i> 
+     
+    </h2>
+    <h3>  
+      ({fruit + 1} of {listFruit.length})
+    </h3>
+    <img className= "w-80"
+    
+      src={tropicalFruit.url} 
+      alt={tropicalFruit.alt}
+    />
+    <p>
+      {tropicalFruit.description}
+    </p>
+  </>
+);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/app/components/listFruit.tsx
@@ -0,0 +1,41 @@
+interface ListFruit{
+    name: string;
+    description : string;
+    url : string;
+    alt : string;
+}
+
+export const listFruit = [{
+    name: "Cajú",
+    description: "mmmmmmm",
+    url:  "https://www.grupoescolar.com/wp-content/uploads/2021/03/356BB.jpg",
+    alt: "Fruta Cajú"
+    },
+
+    {
+        name: "Abacaxi",
+        description: "",
+        url: "https://marolacomcarambola.com.br/wp-content/uploads/2023/03/frutas-nordeste-abacaxi.jpg",
+        alt:  "Fruta Abacaxi"
+    },
+
+    {
+        name: "Manga",
+        description: "",
+        url: "https://cdn.awsli.com.br/600x450/2570/2570929/produto/206770159/muda-de-manga-rosa-enxertia-1-axtcsc.jpg",
+        alt:  "Fruta Manga"
+    },
+
+    {
+        name: "Maracujá",
+        description: "",
+        url: "https://marolacomcarambola.com.br/wp-content/uploads/2023/03/frutas-nordeste-maracuja.jpg",
+        alt:  "Fruta Maracujá",
+    },
+
+    {
+        name: "Cajú",
+        description: "",
+        url: "",
+        alt:  "",
+}];
